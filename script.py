20a8908f2e38ea01b17e14478ed671f228e52a08
remy <remy.gouhier.pro@gmail.com>
2026-02-19 20:57:39 +0100

    FEAT: training the random forest model

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 
 df = pd.read_csv('data/water_potability.csv')
 
@@ -43,3 +44,8 @@ scaler = StandardScaler()
 
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
+
+## Training
+
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X_train_scaled, y_train)
