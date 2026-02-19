5682370a331a479e93c5a521a269e8a4ade5a2e6
remy <remy.gouhier.pro@gmail.com>
2026-02-19 20:52:43 +0100

    FEAT: import dataset and evaluate missing values

---
+++ script.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+df = pd.read_csv('data/water_potability.csv')
+
+#print(df.isnull().sum() / len(df) * 100)
+
+
+#ph                 14.987790
+#Hardness            0.000000
+#Solids              0.000000
+#Chloramines         0.000000
+#Sulfate            23.840049
+#Conductivity        0.000000
+#Organic_carbon      0.000000
+#Trihalomethanes     4.945055
+#Turbidity           0.000000
+#Potability          0.000000
+
