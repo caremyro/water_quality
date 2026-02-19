4eb4a51a463e49340c1986c9fea6f0995806f4b1
remy <remy.gouhier.pro@gmail.com>
2026-02-19 20:56:08 +0100

    FEAT: preprocessing -> split datasets (80/20) train and test sets / applying median of training for missing values in each set

--- script.py
+++ script.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('data/water_potability.csv')
 
@@ -16,3 +18,21 @@ df = pd.read_csv('data/water_potability.csv')
 #Turbidity           0.000000
 #Potability          0.000000
 
+## Preprocessing
+
+X = df.drop('Potability', axis=1)
+y = df['Potability']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+ph_median = X_train['ph'].median()
+sulfate_median = X_train['Sulfate'].median()
+trih_median = X_train['Trihalomethanes'].median()
+
+X_train['ph'] = X_train['ph'].fillna(ph_median)
+X_train['Sulfate'] = X_train['Sulfate'].fillna(sulfate_median)
+X_train['Trihalomethanes'] = X_train['Trihalomethanes'].fillna(trih_median)
+
+X_test['ph'] = X_test['ph'].fillna(ph_median)
+X_test['Sulfate'] = X_test['Sulfate'].fillna(sulfate_median)
+X_test['Trihalomethanes'] = X_test['Trihalomethanes'].fillna(trih_median)
